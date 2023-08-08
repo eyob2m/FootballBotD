6e2060fdb469c1ad137dcb7e7b30ca3ed2296924
eyob2m <eyob2etemariam@gamil.com>
2023-08-09 00:32:56 +0300

    ok

--- bot.js
+++ bot.js
@@ -2,14 +2,8 @@
 const telegraf =  require('telegraf')
 module.exports = bot = new  telegraf("6468224337:AAGrt8G8xYxWtKND6yx5PvtGZgvDP1xKRCU")
 
-const Start =  require('./start')
-Start()
-const About = require('./about')
-About()
-const Table  = require('./tables')
-Table()
-const Live = require('./live')
-Live()
-const Btns = require('./buttons')
-Btns()
+bot.start(ctx=>{
+    ctx.reply("hello")
+})
+
 bot.launch()
