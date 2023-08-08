26754bd2a18097bc1bed5bfc29b0dfb5efda6b7d
eyob2m <eyob2etemariam@gamil.com>
2023-08-08 21:21:38 +0300

    ok

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const t = require('telegraf')
 const bot = new t('6468224337:AAGrt8G8xYxWtKND6yx5PvtGZgvDP1xKRCU')
 
 bot.start(ctx=>{
-    ctx.reply('hello')
+    ctx.reply('hellooo')
 })
 
 bot.launch()
